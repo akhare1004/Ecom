7fbf241a9542070d6c4d01a521a2d5c2893776e8
akhare1004 <animesh1.khare1@gmail.com>
2022-07-18 16:11:15 +0530

    header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
+import { Container } from 'react-bootstrap'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
-
+      <Header />
+      <main className='py-3'>
+        <Container>
+          I am content
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Container, Navbar, Nav } from 'react-bootstrap'
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Nameholder</Navbar.Brand>
+          <Nav className="ml-auto">
+            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+            <Nav.Link href="/login"><i className='fas fa-user'></i> Sign In</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
