c7e65c64b235e9be863c68e385de9513b735c0b2
akhare1004 <animesh1.khare1@gmail.com>
2022-07-22 16:20:18 +0530

    Login Form added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Footer from "./components/Footer";
 import Homescreen from "./screens/Homescreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
   return (
@@ -15,10 +16,11 @@ function App() {
       <main className='py-3'>
         <Container>
           <Routes>
-            <Route path='/' element={<Homescreen />} />
+            <Route path='/login' element={<LoginScreen />} />
             <Route path='/product/:id' element={<ProductScreen />} />
             <Route path='/cart' element={<CartScreen />} />
             <Route path='/cart/:id' element={<CartScreen />} />
+            <Route path='/' element={<Homescreen />} />
           </Routes>
         </Container>
       </main>
